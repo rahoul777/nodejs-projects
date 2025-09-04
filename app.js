9ca432348b351e716a1dcd0b9cf994753c838c8f
Rahul Shambharkar <rahoul777shambharkar@gmail.com>
2025-09-04 05:31:38 +0530

    update app.js Modules and file system

--- app.js
+++ app.js
@@ -60,7 +60,31 @@
 //   console.log("Server running at http://localhost:8000/");
 // });
 
-const { readAndWrite } = require("./fileModule");
+// const { readAndWrite } = require("./fileModule");
+
+// // Call the function
+// readAndWrite();
+// Import fs module
+const fs = require("fs");
+
+// Define function
+function readAndWrite() {
+  fs.readFile("input.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return;
+    }
+
+    fs.writeFile("duplicate.txt", data, (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return;
+      }
+
+      console.log("Content copied to duplicate.txt");
+    });
+  });
+}
 
 // Call the function
 readAndWrite();
